c68c571431e91cd004a5fc806d258b304c928256
aaron taylor <aarontaylor@gmail.com>
2019-02-01 11:32:33 -0800

    add simple front end and add tojs

--- js/script.js
+++ js/script.js
@@ -7,11 +7,11 @@ function Pizza(size, toppings) {
 
 Pizza.prototype.price = function() {
    var p = 3
-   if (this.size == "small") {
+   if (this.size == "Small") {
      p += 1
-   } else if (this.size == "medium") {
-     p += 2.5
-   } else if (this.size == "large") {
+   } else if (this.size == "Medium") {
+     p += 2
+   } else if (this.size == "Large") {
      p += 4
    }
 
@@ -22,9 +22,22 @@ Pizza.prototype.price = function() {
 
 }
 
-
-
 $(document).ready(function() {
 
+  $("#form").submit(function(event){
+      event.preventDefault();
 
+      var s = ($('#size :selected').text());
+
+      var t = []
+
+      $("input:checked[name=topping]").each(function(){
+        t.push($(this).val());
+      });
+
+      var za = new Pizza(s, t)
+
+      alert(za.price())
+
+  });
 });
